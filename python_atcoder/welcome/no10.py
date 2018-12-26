c233358171a387bfb94e24446af22a9b4c571155
dada <dai_fs6gwws4386@outlook.jp>
2018-12-27 00:16:57 +0900

    Revert "Add welcome project"
    This reverts commit 5af3dabd4c6cc908f45d776c7083e2e6bbc5cafc.

--- python_atcoder/welcome/no10.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
- 
-import re, sys
- 
-def main():
-    print("YES") if bool(re.match("^(dream|dreamer|eraser|erase)*$",input())) else print("NO")
-    
-if __name__ == '__main__':
-    main()
--- python_atcoder/welcome/no8.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-def main():
-    N=int(input())
-    print(len(list(set([ int(input()) for i in range(N) ]))))
-    
-if __name__ == '__main__':
-    main()
