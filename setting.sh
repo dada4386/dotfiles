6c72cc90deb0b0c5fc242fd246ff8ed6a8442ed8
dada <dai_fs6gwws4386@outlook.jp>
2019-02-10 22:35:26 +0900

    Add setting.sh

---
+++ setting.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ORIG=`pwd`
+cd ~
+HOME=`pwd`
+
+# vimrc
+VIMRC=".vimrc"
+if [ -e $VIMRC ]; then
+    mv $VIMRC ${VIMRC}.old
+fi
+ln -s $ORIG/$VIMRC $HOME/$VIMRC
+
+# userconfig
+CONFIG=".cache/userconfig"
+if [ -d $CONFIG ]; then
+    mv $CONFIG ${CONFIG}.old
+fi
+ln -s $ORIG/$CONFIG $HOME/$CONFIG
+
