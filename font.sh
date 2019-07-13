f11d0ec5df2d5be01eb92093ee449a87b1155496
Dada <dai_fs6gwws4386@outlook.jp>
2019-07-13 10:35:27 +0900

    フォントをCicaに変更

--- font.sh
+++ font.sh
@@ -1,37 +1,13 @@
 #!/bin/bash
 
-# from https://github.com/tsunesan3/awsome-jp-coding-fonts
+sudo apt install wget -y
+wget https://github.com/miiton/Cica/releases/download/v5.0.1/Cica_v5.0.1_with_emoji.zip
+unzip Cica_v5.0.1_with_emoji.zip
+rm Cica_v5.0.1_with_emoji.zip
 
-sudo apt install python-fontforge fontforge git
-cd
-git clone https://github.com/ryanoasis/nerd-fonts.git
-cd nerd-fonts
-git checkout -f v2.0.0
-cd
-
-wget http://yozvox.web.fc2.com/unitettc.zip
-unzip -o unitettc.zip
-chmod 755 unitettc/unitettc64
-sudo mv unitettc/unitettc64 /usr/local/bin/
-git clone https://github.com/tsunesan3/awsome-jp-coding-fonts.git .fonts
-
-cd ~/nerd-fonts
-git checkout .
-patch -p1 < ~/fonts/tools/font-patcher.diff
-fontforge -script font-patcher ../fonts/RictyDiminished/src/RictyDiminished-Regular.ttf -c
-fontforge -script font-patcher ../fonts/RictyDiminished/src/RictyDiminished-Bold.ttf -c
-fontforge -script font-patcher ../fonts/RictyDiminished/src/RictyDiminished-Oblique.ttf -c
-fontforge -script font-patcher ../fonts/RictyDiminished/src/RictyDiminished-BoldOblique.ttf -c
-unitettc64 RictyDiminished.ttc RictyDiminished-{Regular,Bold,Oblique,BoldOblique}.ttf
-rm Ricty*.ttf
-fontforge -script font-patcher ../fonts/RictyDiminished/src/RictyDiminishedDiscord-Regular.ttf -c
-fontforge -script font-patcher ../fonts/RictyDiminished/src/RictyDiminishedDiscord-Bold.ttf -c
-fontforge -script font-patcher ../fonts/RictyDiminished/src/RictyDiminishedDiscord-Oblique.ttf -c
-fontforge -script font-patcher ../fonts/RictyDiminished/src/RictyDiminishedDiscord-BoldOblique.ttf -c
-unitettc64 RictyDiminishedDiscord.ttc RictyDiminishedDiscord-{Regular,Bold,Oblique,BoldOblique}.ttf
-rm Ricty*.ttf
-mv RictyDiminished*.ttc ../fonts/RictyDiminished/
+mkdir ~/.fonts/Cica
+mv Cica-* ~/.fonts/Cica
 
 fc-cache -fv
 UUID=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \')
-gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${UUID}/ font "RictyDiminished 12"
+gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${UUID}/ font "Cica 12"
