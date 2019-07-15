b456793969d1ad04422e2fec607cb3b200cb2b8b
Dada <dai_fs6gwws4386@outlook.jp>
2019-07-15 10:42:07 +0900

    Revert "コマンド存在判定が正常に動作しないので修正"
    This reverts commit 4369f5db50f5d72449a1edacc1e104f0ca882e51.

--- font.sh
+++ font.sh
@@ -8,8 +8,8 @@ rm Cica_v5.0.1_with_emoji.zip
 mkdir -p ~/.fonts/Cica
 mv Cica-* ~/.fonts/Cica
 
-# fc-cache -fv
-if [ -n "`which gsettings`" ]; then
+fc-cache -fv
+if [ -n `which gsettings` ]; then
     UUID=$(gsettings get org.gnome.Terminal.ProfilesList default | tr -d \')
     gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:${UUID}/ font "Cica 12"
 fi
