d05c1d46b6dee5cd7778d40c7631c0405546e5de
Dada <dai_fs6gwws4386@outlook.jp>
2019-07-15 17:09:44 +0900

    Add package software-properties-common

--- vim_install.sh
+++ vim_install.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo apt-get remove --purge vim vim-runtime vim-common
-sudo apt-get install -y git build-essential ncurses-dev lua5.2 lua5.2-dev luajit python-dev python3-dev ruby-dev python3-pip python-setuptools
+sudo apt-get install -y git build-essential ncurses-dev lua5.2 lua5.2-dev luajit python-dev \
+    python3-dev ruby-dev python3-pip python-setuptools software-properties-common
 pip3 install --upgrade neovim
 
 sudo add-apt-repository -y ppa:neovim-ppa/stable
