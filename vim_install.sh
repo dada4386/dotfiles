76ce9d59cf954f8d92011988188ec4f49298f69a
latona <daiki.m@latona.io>
2019-11-05 11:53:00 +0900

    Added installed softwares

--- vim_install.sh
+++ vim_install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 sudo apt-get install -y git build-essential ncurses-dev lua5.2 lua5.2-dev luajit python-dev \
-    python3-dev ruby-dev python3-pip python-setuptools software-properties-common
-pip3 install --upgrade neovim
+    python3-dev ruby-dev python3-pip python-setuptools software-properties-common npm
+pip3 install --upgrade --user neovim pynvim powerline-status
 
 sudo add-apt-repository -y ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install neovim -y
+
+sudo npm install --global neovim
 
 sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 sudo update-alternatives --config vi
