fb879b08601b2f464d58bfd5ebc0fc71fbc6cf32
extdada <dai_fs6gwws4386@outlook.jp>
2019-12-13 14:14:42 +0900

    Added saving session for tmux

--- vim_install.sh
+++ vim_install.sh
@@ -4,6 +4,8 @@ sudo apt-get install -y git build-essential ncurses-dev lua5.2 lua5.2-dev luajit
     python3-dev ruby-dev python3-pip python-setuptools software-properties-common npm
 pip3 install --upgrade --user neovim pynvim powerline-status cpplint flake8 autopep8 isort
 
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 sudo add-apt-repository -y ppa:neovim-ppa/stable
 sudo apt-get update
 sudo apt-get install neovim -y
@@ -14,4 +16,3 @@ sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
 sudo update-alternatives --config vi
 sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
 sudo update-alternatives --config vim
-
